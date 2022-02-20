58f1af3a1101a44e7309de123f194b9ca025e5d0
amin baratian <aminbaratian73@gmail.com>
2022-02-20 23:09:24 +0330

    catboost_info is ignored.

--- examples/MSClst_example.py
+++ examples/MSClst_example.py
@@ -5,7 +5,6 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.metrics import silhouette_score
 
 # Loading data
-    
 rand = int(np.random.uniform(1,1000))
     
 data, true_labels = make_blobs(n_samples=[300,350,250], 
@@ -13,16 +12,16 @@ data, true_labels = make_blobs(n_samples=[300,350,250],
 
 file = pd.DataFrame(data, columns=["x1", "x2", "x3", "x4"])
 #file.plot.scatter("x1", "x2")
-
+#print(file)
 X = file.iloc[: , :].values
 
 bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
 
 Model = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    
+
 Model.fit(X)
 labels = Model.labels_
-
+print(type(Model))
 clustring_silhouette_score = silhouette_score(X, labels)
 print(clustring_silhouette_score)
 
